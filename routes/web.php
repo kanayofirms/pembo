362fa69da496173791f17c70f6af81ae253ff139
kanayofirms <kanyeze@gmail.com>
2024-08-06 12:52:46 +0100

    Create GET routes for register and forgot password

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'login']);
+Route::get('register', [AuthController::class, 'register']);
+Route::get('forgot-password', [AuthController::class, 'forgot-password']);
 
