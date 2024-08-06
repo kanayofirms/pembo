090ff9ebe3c5a8883b8a225a1c4e620d15beaec1
kanayofirms <kanyeze@gmail.com>
2024-08-06 22:53:20 +0100

    Create GET route for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'register']);
 Route::get('forgot_password', [AuthController::class, 'forgot_password']);
+
+Route::get('admin/dashboard', [DashboardController::class, 'index']);
 
