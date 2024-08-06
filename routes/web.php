1b2d0758cdf185f1ac568ce59c26b80a5d85ee2d
kanayofirms <kanyeze@gmail.com>
2024-08-06 20:56:14 +0100

    Refactor forgot password route for readability

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'register']);
-Route::get('forgot-password', [AuthController::class, 'forgot-password']);
+Route::get('forgot_password', [AuthController::class, 'forgot_password']);
 
