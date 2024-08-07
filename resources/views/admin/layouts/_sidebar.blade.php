a4ef4bd7dca90023d7ecee22321ecde135bb291e
kanayofirms <kanyeze@gmail.com>
2024-08-07 08:07:10 +0100

    Set active menu for dashboard and staff

--- resources/views/admin/layouts/_sidebar.blade.php
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -4,14 +4,16 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-            <a class="nav-link " href="{{ url('admin/dashboard') }}">
+            <a class="nav-link @if (Request::segment(2) == 'dashboard') @else collapsed @endif"
+                href="{{ url('admin/dashboard') }}">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li><!-- End Dashboard Nav -->
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="{{ url('admin/staff/list') }}">
+            <a class="nav-link @if (Request::segment(2) == 'staff') @else collapsed @endif"
+                href="{{ url('admin/staff/list') }}">
                 <i class="bi bi-person"></i>
                 <span>Staff</span>
             </a>
