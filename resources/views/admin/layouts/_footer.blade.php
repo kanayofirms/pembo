a9dd754ebdf56308189d5cf6e80a32b7ae401820
kanayofirms <kanyeze@gmail.com>
2024-08-07 07:08:47 +0100

    Create layouts directory for views on admin side

---
+++ resources/views/admin/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Pembo Management System</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+
+        Designed by <a target="_blank" href="https://pembo.org/">Pembo</a>
+    </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
