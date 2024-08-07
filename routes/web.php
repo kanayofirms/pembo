5a5353383bf9892d2c7323bda1d37a4fd3c0716e
kanayofirms <kanyeze@gmail.com>
2024-08-07 11:10:21 +0100

    Create POST route for insert register record

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\StaffController;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'register']);
+Route::post('register_post', [AuthController::class, 'register_post']);
 Route::get('forgot_password', [AuthController::class, 'forgot_password']);
 
 Route::get('admin/dashboard', [DashboardController::class, 'index']);
