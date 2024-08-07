43bd828441eb8026ee16ff215a5649ec596f2e8a
kanayofirms <kanyeze@gmail.com>
2024-08-07 11:09:20 +0100

    Create function for insert register record into the db

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Str;
+use Hash;
 
 class AuthController extends Controller
 {
@@ -19,5 +22,19 @@ class AuthController extends Controller
     public function forgot_password(Request $request)
     {
         return view('auth.forgot_password');
+    }
+
+    public function register_post(Request $request)
+    {
+        // dd($request->all());
+        $user = new User;
+        $user->name = trim($request->name);
+        $user->last_name = trim($request->last_name);
+        $user->email = trim($request->email);
+        $user->password = Hash::make($request->password);
+        $user->remember_token = Str::random(50);
+        $user->save();
+
+        return redirect('/')->with('success', "Registeration successfully done.");
     }
 }
