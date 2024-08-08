6e9e20a7215fb4cc2709c6acffc0e31f9d24b576
kanayofirms <kanyeze@gmail.com>
2024-08-08 15:30:57 +0100

    Refactor index function for feasibility

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('admin.dashboard.list');
+        if (Auth::user()->is_role == 1) { //admin authorization
+            return view('admin.dashboard.list');
+        } else if (Auth::user()->is_role == 0) { //staff authorization
+            return view('admin.staff.list');
+        }
     }
 }
