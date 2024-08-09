cb3e99a69b8c4406c28aa6e7c326e5d63b48e796
kanayofirms <kanyeze@gmail.com>
2024-08-09 03:42:52 +0100

    Refactor code with action url and method

--- resources/views/admin/staff/add.blade.php
+++ resources/views/admin/staff/add.blade.php
@@ -14,51 +14,52 @@
                         <h5 class="card-title">Staff</h5>
 
                         <!-- General Form Elements -->
-                        <form>
+                        <form action="{{ url('admin/staff/add') }}" method="POST" enctype="multipart/form-data">
+                            {{ csrf_field() }}
                             <div class="row mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">First Name</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" name="name">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">Last Name</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" name="last_name">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">Surname</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control">
+                                    <input type="text" class="form-control" name="surname">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                                 <div class="col-sm-10">
-                                    <input type="email" class="form-control">
+                                    <input type="email" class="form-control" name="email">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputNumber" class="col-sm-2 col-form-label">Phone Number</label>
                                 <div class="col-sm-10">
-                                    <input type="number" class="form-control">
+                                    <input type="number" class="form-control" name="phone_number">
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="inputNumber" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" type="file" id="formFile">
+                                    <input class="form-control" type="file" id="formFile" name="profile_image">
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="inputDate" class="col-sm-2 col-form-label">DOB</label>
                                 <div class="col-sm-10">
-                                    <input type="date" class="form-control">
+                                    <input type="date" class="form-control" name="date_of_birth">
                                 </div>
                             </div>
 
@@ -67,7 +68,7 @@
                             <div class="row mb-3">
                                 <label class="col-sm-2 col-form-label">Role</label>
                                 <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example">
+                                    <select class="form-select" name="is_role" aria-label="Default select example">
                                         <option selected>Select Role Menu</option>
                                         <option value="0">Staff</option>
                                         <option value="1">Admin</option>
