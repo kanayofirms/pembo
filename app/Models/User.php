081cb740c667e9edd00a596b98f7fb46e03fea0c
kanayofirms <kanyeze@gmail.com>
2024-08-09 01:19:55 +0100

    Create function to get all user records

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    static public function getAllRecord()
+    {
+        return self::get();
+    }
 }
