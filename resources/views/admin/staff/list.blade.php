c072e46f3647566964bcd496146466eabba8f2d1
kanayofirms <kanyeze@gmail.com>
2024-08-09 03:44:31 +0100

    Include message for feasibility

--- resources/views/admin/staff/list.blade.php
+++ resources/views/admin/staff/list.blade.php
@@ -9,6 +9,7 @@
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
+                @include('_message')
 
                 <div class="card">
                     <div class="card-body">
