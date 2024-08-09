5583197bd01dea85833a21154b7b23d1178597a1
kanayofirms <kanyeze@gmail.com>
2024-08-09 01:23:48 +0100

    Create add function for add staff record

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class StaffController extends Controller
 {
     public function index(Request $request)
     {
-        return view('admin.staff.list');
+        $data['getRecord'] = User::getAllRecord();
+        return view('admin.staff.list', $data);
+    }
+
+    public function add(Request $request)
+    {
+        return view('admin.staff.add');
     }
 }
