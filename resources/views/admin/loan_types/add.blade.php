90d231ce3904e2f762abfcc8827ce6dfed9b7292
kanayofirms <kanyeze@gmail.com>
2024-08-10 20:47:03 +0100

    Create input types and update app

--- resources/views/admin/loan_types/add.blade.php
+++ resources/views/admin/loan_types/add.blade.php
@@ -1,4 +1,49 @@
 @extends('admin.layouts.app')
 
 @section('content')
+    <div class="pagetitle">
+        <h1>Add Loan Type</h1>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Loan Type</h5>
+                        <!-- General Form Elements -->
+                        <form action="{{ url('admin/loan_types/add') }}" method="POST" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="row mb-3">
+                                <label for="inputText" class="col-sm-2 col-form-label">Type Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="type_name"
+                                        value="{{ old('type_name') }}" required>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="inputText" class="col-sm-2 col-form-label">Description</label>
+                                <div class="col-sm-10">
+                                    <textarea type="text" class="form-control" name="description" {{ old('description') }}></textarea>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+
+                        </form><!-- End General Form Elements -->
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
 @endsection
