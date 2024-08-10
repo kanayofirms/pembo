bad1e6fc23d18cdffb622696446a5b7ad43a397f
kanayofirms <kanyeze@gmail.com>
2024-08-10 08:13:06 +0100

    Refactor code for scalability

--- resources/views/admin/staff/edit.blade.php
+++ resources/views/admin/staff/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="pagetitle">
-        <h1>Add Staff</h1>
+        <h1>Edit Staff</h1>
     </div><!-- End Page Title -->
 
     <section class="section">
@@ -14,40 +14,49 @@
                         <h5 class="card-title">Staff</h5>
 
                         <!-- General Form Elements -->
-                        <form action="{{ url('admin/staff/add') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ url('admin/staff/edit/' . $getRecord->id) }}" method="POST"
+                            enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div class="row mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">First Name</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="name">
+                                    <input type="text" class="form-control" name="name" value="{{ $getRecord->name }}"
+                                        required>
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">Last Name</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="last_name">
+                                    <input type="text" class="form-control" name="last_name"
+                                        value="{{ $getRecord->last_name }}">
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
+                            <div class="row
+                                        mb-3">
                                 <label for="inputText" class="col-sm-2 col-form-label">Surname</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="surname">
+                                    <input type="text" class="form-control" name="surname"
+                                        value="{{ $getRecord->surname }}">
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
+                            <div class="row
+                                            mb-3">
                                 <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                                 <div class="col-sm-10">
-                                    <input type="email" class="form-control" name="email">
+                                    <input type="email" class="form-control" name="email"
+                                        value="{{ $getRecord->email }}">
+                                    <span style="color: red;">{{ $errors->first('email') }}</span>
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="inputNumber" class="col-sm-2 col-form-label">Phone Number</label>
                                 <div class="col-sm-10">
-                                    <input type="number" class="form-control" name="phone_number">
+                                    <input type="number" class="form-control" name="phone_number"
+                                        value="{{ $getRecord->phone_number }}">
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -59,7 +68,8 @@
                             <div class="row mb-3">
                                 <label for="inputDate" class="col-sm-2 col-form-label">DOB</label>
                                 <div class="col-sm-10">
-                                    <input type="date" class="form-control" name="date_of_birth">
+                                    <input type="date" class="form-control" name="date_of_birth"
+                                        value="{{ $getRecord->date_of_birth }}">
                                 </div>
                             </div>
 
@@ -70,9 +80,10 @@
                                 <div class="col-sm-10">
                                     <select class="form-select" name="is_role" aria-label="Default select example">
                                         <option selected>Select Role Menu</option>
-                                        <option value="0">Staff</option>
-                                        <option value="1">Admin</option>
-                                        <option value="3">Three</option>
+                                        <option {{ $getRecord->is_role == '0' ? 'selected' : '' }} value="0">Staff
+                                        </option>
+                                        <option {{ $getRecord->is_role == '1' ? 'selected' : '' }} value="1">Admin
+                                        </option>
                                     </select>
                                 </div>
                             </div>
@@ -81,7 +92,7 @@
                             <div class="row mb-3">
                                 <label class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-primary">Update</button>
                                 </div>
                             </div>
 
