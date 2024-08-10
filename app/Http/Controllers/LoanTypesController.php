c53de3ce531504cc3790a77fbec92af789714476
kanayofirms <kanyeze@gmail.com>
2024-08-10 17:14:21 +0100

    Create store function for insert add loan types into the db

--- app/Http/Controllers/LoanTypesController.php
+++ app/Http/Controllers/LoanTypesController.php
@@ -16,4 +16,17 @@ class LoanTypesController extends Controller
     {
         return view('admin.loan_types.add');
     }
+
+    public function store(Request $request)
+    {
+        $save = request()->validate([
+            'type_name' => 'required'
+        ]);
+        $save = new LoanTypesModel;
+        $save->type_name = trim($request->type_name);
+        $save->description = trim($request->description);
+        $save->save();
+
+        return redirect('admin/loan_types/list')->with('success', "Loan Types Successfully Created.");
+    }
 }
