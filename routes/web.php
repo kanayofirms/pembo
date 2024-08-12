42668f2013fd6d330a3a7d4dd52716dfcf670d70
kanayofirms <kanyeze@gmail.com>
2024-08-12 23:59:17 +0100

    Create POST route for store loan user record in db

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,7 @@ Route::group(['middleware' => 'admin'], function () {
     //Loan User
     Route::get('admin/loan_user/list', [LoanUserController::class, 'index']);
     Route::get('admin/loan_user/add', [LoanUserController::class, 'create']);
+    Route::post('admin/loan_user/add', [LoanUserController::class, 'store']);
 });
 
 Route::group(['middleware' => 'staff'], function () {
