48d000a56c350363df62036bcf2ddf3e4c9374d5
kanayofirms <kanyeze@gmail.com>
2024-08-12 20:37:59 +0100

    Create active menu for loan user on admin side

--- resources/views/admin/layouts/_sidebar.blade.php
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -44,6 +44,14 @@
         </li><!-- End Staff Page Nav -->
 
         <li class="nav-item">
+            <a class="nav-link @if (Request::segment(2) == 'loan_user') @else collapsed @endif"
+                href="{{ url('admin/loan_user/list') }}">
+                <i class="bi bi-person"></i>
+                <span>Loan User</span>
+            </a>
+        </li><!-- End Staff Page Nav -->
+
+        <li class="nav-item">
             <a class="nav-link collapsed" href="">
                 <i class="bi bi-person"></i>
                 <span>Profile</span>
