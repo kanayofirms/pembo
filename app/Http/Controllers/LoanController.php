b6673feea6cca44c6138ed4412169e545978832f
kanayofirms <kanyeze@gmail.com>
2024-08-12 14:03:35 +0100

    Get loan types record from loan types model and pass data to view

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\LoanModel;
+use App\Models\LoanTypesModel;
 
 class LoanController extends Controller
 {
@@ -22,6 +23,7 @@ class LoanController extends Controller
 
     public function create(Request $request)
     {
-        return view('admin.loan.create');
+        $data['getLoanTypes'] = LoanTypesModel::get();
+        return view('admin.loan.create', $data);
     }
 }
