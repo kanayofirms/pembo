23b7734a899854a9985cb97a919c6d1fbd45da96
kanayofirms <kanyeze@gmail.com>
2024-08-13 23:05:50 +0100

    Refactor dropdowns with ternary operator for feasibility

--- resources/views/admin/loan/edit.blade.php
+++ resources/views/admin/loan/edit.blade.php
@@ -14,7 +14,8 @@
                         <h5 class="card-title">Edit Loan</h5>
 
                         <!-- General Form Elements -->
-                        <form action="{{ url('admin/loan/add') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ url('admin/loan/edit/' . $getRecord->id) }}" method="POST"
+                            enctype="multipart/form-data">
                             {{ csrf_field() }}
 
                             <div class="row mb-3">
@@ -23,7 +24,9 @@
                                     <select class="form-select" name="user_id" required>
                                         <option value="">Select Borrower</option>
                                         @foreach ($getLoanUser as $value_0)
-                                            <option value="{{ $value_0->id }}">{{ $value_0->first_name }}
+                                            <option {{ $value_0->id == $getRecord->user_id ? 'selected' : '' }}
+                                                value="{{ $value_0->id }}">
+                                                {{ $value_0->first_name }}
                                                 {{ $value_0->middle_name }} {{ $value_0->surname }}</option>
                                         @endforeach
                                     </select>
@@ -36,7 +39,8 @@
                                     <select class="form-select" name="staff_id" required>
                                         <option value="">Select Staff</option>
                                         @foreach ($getStaff as $value_1)
-                                            <option value="{{ $value_1->id }}">{{ $value_1->name }}
+                                            <option {{ $value_1->id == $getRecord->staff_id ? 'selected' : '' }}
+                                                value="{{ $value_1->id }}">{{ $value_1->name }}
                                                 {{ $value_1->last_name }} {{ $value_1->surname }}</option>
                                         @endforeach
                                     </select>
@@ -49,7 +53,8 @@
                                     <select class="form-select" name="loan_types_id" required>
                                         <option value="">Select Loan Types</option>
                                         @foreach ($getLoanTypes as $value_2)
-                                            <option value="{{ $value_2->id }}">{{ $value_2->type_name }}</option>
+                                            <option {{ $value_2->id == $getRecord->loan_types_id ? 'selected' : '' }}
+                                                value="{{ $value_2->id }}">{{ $value_2->type_name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -61,7 +66,8 @@
                                     <select class="form-select" name="loan_plan_id" required>
                                         <option value="">Select Loan Plan</option>
                                         @foreach ($getLoanPlan as $value_3)
-                                            <option value="{{ $value_3->id }}">{{ $value_3->months }}
+                                            <option {{ $value_3->id == $getRecord->loan_plan_id ? 'selected' : '' }}
+                                                value="{{ $value_3->id }}">{{ $value_3->months }}
                                                 [ {{ $value_3->interest_percent . '% ' . $value_3->penalty_rate }} ]
                                             </option>
                                         @endforeach
@@ -74,7 +80,7 @@
                                 <label for="inputNumber" required class="col-sm-2 col-form-label">Loan Amount</label>
                                 <div class="col-sm-10">
                                     <input required type="number" class="form-control" name="loan_amount"
-                                        value="{{ old('loan_amount') }}"
+                                        value="{{ $getRecord->loan_amount }}"
                                         oninput="javascript: this.value = this.value.replace(/[^0-9]/g, ''); if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLenght);"
                                         maxlength="10">
                                 </div>
@@ -83,7 +89,7 @@
                             <div class="row mb-3">
                                 <label for="inputNumber" class="col-sm-2 col-form-label">Purpose</label>
                                 <div class="col-sm-10">
-                                    <textarea name="purpose" class="form-control"></textarea>
+                                    <textarea name="purpose" class="form-control">{{ $getRecord->purpose }}</textarea>
                                 </div>
                             </div>
 
