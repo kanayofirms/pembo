fec9e1b6df3891c845df537f882e1cc128266ac3
kanayofirms <kanyeze@gmail.com>
2024-08-13 09:45:41 +0100

    Fetch records dynamically and list on view

--- resources/views/admin/loan/index.blade.php
+++ resources/views/admin/loan/index.blade.php
@@ -18,7 +18,48 @@
                         </h5>
 
                         <!-- Table with stripped rows -->
-
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>
+                                        User Name
+                                    </th>
+                                    <th>Staff Name</th>
+                                    <th>Loan Type</th>
+                                    <th>Loan Plan</th>
+                                    <th>Loan Amount</th>
+                                    <th>Purpose</th>
+                                    <th>Created Date</th>
+                                    <th>Updated Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($getRecord as $value)
+                                    <tr>
+                                        <th>{{ $value->id }}</th>
+                                        <th>{{ $value->user_id }}</th>
+                                        <th>{{ $value->staff_id }}</th>
+                                        <th>{{ $value->loan_types_id }}</th>
+                                        <th>{{ $value->loan_plan_id }}</th>
+                                        <th>{{ $value->loan_amount }}</th>
+                                        <th>{{ $value->purpose }}</th>
+                                        <td>{{ date('d-m-Y', strtotime($value->created_at)) }}</td>
+                                        <td>{{ date('d-m-Y', strtotime($value->updated_at)) }}</td>
+                                        <td>
+                                            <a href="{{ url('admin/loan_plan/edit/' . $value->id) }}"
+                                                class="btn btn-success"><i class="bi bi-pencil-square"></i>
+                                            </a>
+                                            <a href="{{ url('admin/loan_plan/delete/' . $value->id) }}"
+                                                onclick="return confirm('Are You Sure You Want To Delete?')"
+                                                class="btn btn-danger"><i class="bi bi-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                         <!-- End Table with stripped rows -->
 
                     </div>
