0734a1004ef686a4e68c84dae4a24e49e666a9f8
kanayofirms <kanyeze@gmail.com>
2024-08-15 04:00:37 +0100

    Create inputs and refactor code for scalability

--- resources/views/admin/profile/update.blade.php
+++ resources/views/admin/profile/update.blade.php
@@ -8,7 +8,7 @@
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
-
+                @include('_message')
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Profile</h5>
