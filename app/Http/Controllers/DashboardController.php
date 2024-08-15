21782292e82fa0ea99aced4e14687725a13a935f
kanayofirms <kanyeze@gmail.com>
2024-08-15 04:27:18 +0100

    Get count for staff and admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,10 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         if (Auth::user()->is_role == 1) { //admin authorization
-            return view('admin.dashboard.list');
+            $data['getStaffandAdminCount'] = User::count();
+
+
+            return view('admin.dashboard.list', $data);
         } else if (Auth::user()->is_role == 0) { //staff authorization
             return view('admin.staff.list');
         }
