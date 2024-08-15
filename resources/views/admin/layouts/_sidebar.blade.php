61454852cab55857241bc672ce8ba28a927ce076
kanayofirms <kanyeze@gmail.com>
2024-08-15 03:48:44 +0100

    Refactor profile menu for feasibility

--- resources/views/admin/layouts/_sidebar.blade.php
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -52,7 +52,8 @@
         </li><!-- End Staff Page Nav -->
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="{{ url('admin/profile') }}">
+            <a class="nav-link @if (Request::segment(2) == 'loan_user') @else collapsed @endif"
+                href="{{ url('admin/profile') }}">
                 <i class="bi bi-person"></i>
                 <span>Profile</span>
             </a>
