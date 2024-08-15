7791706e72116b750a18c1fa529e0d1f6298ac3e
kanayofirms <kanyeze@gmail.com>
2024-08-15 05:10:53 +0100

    Fetch and display loan count dynamically

--- resources/views/admin/dashboard/list.blade.php
+++ resources/views/admin/dashboard/list.blade.php
@@ -84,6 +84,30 @@
                         </div>
                     </div><!-- End Revenue Card -->
 
+                    <!-- Sales Card -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card info-card sales-card">
+
+
+
+                            <div class="card-body">
+                                <h5 class="card-title">Loan</h5>
+
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-people"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{ $getLoanCount }}</h6>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div><!-- End Sales Card -->
+
+
                     <!-- Customers Card -->
                     <!-- End Customers Card -->
 
