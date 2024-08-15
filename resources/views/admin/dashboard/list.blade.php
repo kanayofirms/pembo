6f1a11a84c4532576b1cacdfbff51b6a7f173a95
kanayofirms <kanyeze@gmail.com>
2024-08-15 04:33:52 +0100

    Fetch and display loan type and loan plan count dynamically

--- resources/views/admin/dashboard/list.blade.php
+++ resources/views/admin/dashboard/list.blade.php
@@ -51,12 +51,35 @@
 
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-currency-dollar"></i>
+                                        <i class="bi bi-archive"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>$3,264</h6>
-                                        <span class="text-success small pt-1 fw-bold">8%</span> <span
-                                            class="text-muted small pt-2 ps-1">increase</span>
+                                        <h6>{{ $getLoanTypeCount }}</h6>
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div><!-- End Revenue Card -->
+
+                    <!-- Revenue Card -->
+                    <div class="col-xxl-4 col-md-6">
+                        <div class="card info-card revenue-card">
+
+
+
+                            <div class="card-body">
+                                <h5 class="card-title">Loan Plan</h5>
+
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-archive"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{ $getLoanPlanCount }}</h6>
+
 
                                     </div>
                                 </div>
