1de5ecacb357ee991605ed2c967b535f220f3c1e
kanayofirms <kanyeze@gmail.com>
2024-08-22 12:42:57 +0100

    Fix maxlength validation for phone number

--- resources/views/admin/staff/add.blade.php
+++ resources/views/admin/staff/add.blade.php
@@ -54,10 +54,11 @@
                                 <div class="col-sm-10">
                                     <input type="number" class="form-control" name="phone_number"
                                         value="{{ old('phone_number') }}"
-                                        oninput="javascript: this.value = this.value.replace(/[^0-9]/g, ''); if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLenght);"
-                                        maxlength="10">
+                                        oninput="javascript: this.value = this.value.replace(/[^0-9]/g, ''); if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                                        maxlength="11">
                                 </div>
                             </div>
+
                             <div class="row mb-3">
                                 <label for="inputNumber" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
