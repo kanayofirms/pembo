7f7a2c3c8956191ce9b2eca0ba99486f393f2a1c
kanayofirms <kanyeze@gmail.com>
2024-08-22 14:43:07 +0100

    Fetch and display single user record dynamically

--- resources/views/admin/staff/view.blade.php
+++ resources/views/admin/staff/view.blade.php
@@ -16,6 +16,84 @@
                             <a href="{{ url('admin/staff/add') }}" class="btn btn-primary">Staff</a>
                         </h5>
 
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Staff ID</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->id }}</div>
+                        </div>
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">First Name</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->name }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Last Name</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->last_name }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Surame</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->surname }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Email</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->email }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Phone Number</div>
+                            <div class="col-lg-9 col-md-8">{{ $getRecord->phone_number }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Profile Image</div>
+                            <div class="col-lg-9 col-md-8">
+                                @if (!empty($getRecord->profile_image))
+                                    <img src="{{ $getRecord->getProfileImage() }}" alt="Profile Image" height="100px"
+                                        width="100px">
+                                @endif
+                            </div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Date Of Birth</div>
+                            <div class="col-lg-9 col-md-8">{{ date('d-m-Y', strtotime($getRecord->date_of_birtht)) }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Role</div>
+                            <div class="col-lg-9 col-md-8">{{ !empty($getRecord->is_role) ? 'admin' : 'staff' }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Created Date</div>
+                            <div class="col-lg-9 col-md-8">{{ date('d-m-Y', strtotime($getRecord->created_at)) }}</div>
+                        </div>
+
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 label">Updated Date</div>
+                            <div class="col-lg-9 col-md-8">{{ date('d-m-Y', strtotime($getRecord->updated_at)) }}</div>
+                        </div>
 
                     </div>
                 </div>
