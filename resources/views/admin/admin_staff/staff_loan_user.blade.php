c30e5b4130723ba941bf72302b7fc3f2b96c5399
kanayofirms <kanyeze@gmail.com>
2024-08-23 09:35:49 +0100

    Refactor code for functionality

--- resources/views/admin/admin_staff/staff_loan_user.blade.php
+++ resources/views/admin/admin_staff/staff_loan_user.blade.php
@@ -1,3 +1,72 @@
 @extends('admin.layouts.app')
 @section('content')
+    <div class="pagetitle">
+        <h1>Loan User List</h1>
+
+    </div><!-- End Page Title -->
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                @include('_message')
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                        </h5>
+
+                        <!-- Table with stripped rows -->
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>
+                                        User Name
+                                    </th>
+                                    <th>Staff Name</th>
+                                    <th>Loan Type</th>
+                                    <th>Loan Plan</th>
+                                    <th>Loan Amount</th>
+                                    <th>Purpose</th>
+                                    <th>Created Date</th>
+                                    <th>Updated Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {{-- @foreach ($getRecord as $value)
+                                    <tr>
+                                        <th>{{ $value->id }}</th>
+                                        <th>{{ $value->getUserName->first_name }} {{ $value->getUserName->middle_name }}
+                                            {{ $value->getUserName->surname }}</th>
+                                        <th>{{ $value->getStaffName->name }} {{ $value->getStaffName->last_name }}
+                                            {{ $value->getStaffName->surname }}</th>
+                                        <th>{{ $value->getLoanType->type_name }}</th>
+                                        <th>{{ $value->getLoanPlan->months . ' [' . $value->getLoanPlan->interest_percent . '% ' . $value->getLoanPlan->penalty_rate . ']' }}
+                                        </th>
+                                        <th>{{ $value->loan_amount }}</th>
+                                        <th>{{ $value->purpose }}</th>
+                                        <td>{{ date('d-m-Y', strtotime($value->created_at)) }}</td>
+                                        <td>{{ date('d-m-Y', strtotime($value->updated_at)) }}</td>
+                                        <td>
+                                            <a href="{{ url('admin/loan/edit/' . $value->id) }}" class="btn btn-success"><i
+                                                    class="bi bi-pencil-square"></i>
+                                            </a>
+                                            <a href="{{ url('admin/loan/delete/' . $value->id) }}"
+                                                onclick="return confirm('Are You Sure You Want To Delete?')"
+                                                class="btn btn-danger"><i class="bi bi-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach --}}
+                            </tbody>
+                        </table>
+                        <!-- End Table with stripped rows -->
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 @endsection
